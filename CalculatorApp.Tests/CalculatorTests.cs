21c14881f17812afaf236b57394d7f4192a340b9
whtduck1 <myszormateusz02@gmail.com>
2026-06-04 14:15:41 +0200

    red

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using CalculatorApp
+
+namespace CalculatorApp.Tests;
+
+public class CalculatorTests  
+{
+    [Fact]
+    public void ShouldAddTwoNumbers()
+    {
+        var calculator = new Calculator();
+        double result = calculator.Add(1, 2);
+        Assert.Equal(3, result);
+    }
+}
